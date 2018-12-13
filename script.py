331b02b838765aa16e21f59e607cda264d411915
Ramon Fried <ramon.fried@gmail.com>
2018-12-13 07:25:09 +0200

    2
    Signed-off-by: Ramon Fried <ramon.fried@gmail.com>

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 #!/bin/env python3
 from PIL import Image, ImageFont
-import textwrap
 from pathlib import Path
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz'
 
 def xor_strings(s, t):
     """xor two strings together"""
@@ -40,12 +38,6 @@ def add_strings(s, t):
     else:
         return bytes([a & b for a, b in zip(s, t)])
 
-def Rotate(key: int) -> str:
-    rotate = ""
-    for l in alphabet:
-        if l in alphabet:
-             rotate += alphabet[(alphabet.index(l) + key) % (len(alphabet))]
-    return rotate
 
 def extract_pixels(imgPath_orig, imgPath_decode, start, end):
     image1 = Image.open(imgPath_orig)
@@ -59,92 +51,62 @@ def extract_pixels(imgPath_orig, imgPath_decode, start, end):
         cipher2 += chr(red_band2.getpixel((i, 0)))
     return cipher1, cipher2
 
+#        value_altered = ''.join(chr((ord(letter)+i) % 127)  for letter in a)
 
+def print_inc(a, i):
+    value_altered = ''.join(chr((ord(letter)+i) % 127)  for letter in a)
+    return value_altered
 
-#   xor_string = xor_strings(cipher1, cipher2)
-#    print(xor_string)
-#    for key in range(44):
-#        Rotate(key)
-#        table = str.maketrans(Rotate(key), alphabet)
-#        print(xor_string.translate(table))
-#
-#    print("")
-#    for i in range(start,end):
-#        if red_band1.getpixel((i, 0)) != red_band2.getpixel((i, 0)):
-#                print(chr(red_band1.getpixel((i, 0)) | red_band2.getpixel((i, 0))), end='')
-#    print("")
-#    for i in range(start,end):
-#        if red_band1.getpixel((i, 0)) != red_band2.getpixel((i, 0)):
-#                print(chr(red_band1.getpixel((i, 0)) & red_band2.getpixel((i, 0))), end='')
-#    print("")
-#
-#    for i in range(start,end):
-#        if red_band1.getpixel((i, 0)) != red_band2.getpixel((i, 0)):
-#                print(chr(red_band1.getpixel((i, 0)) + red_band2.getpixel((i, 0))), end='')
-#
-#    print("")
-#    for i in range(start,end):
-#        if red_band1.getpixel((i, 0)) != red_band2.getpixel((i, 0)):
-#                print(chr(red_band2.getpixel((i, 0))), end='')
-    print("")
+    # str_list = list(value_altered)
+    # for b in range(i - 1, len(str_list), i):
+    #     print(b)
+    #     str_list[b] = chr(ord(str_list[b]) - 2 )
+    # print("".join(str_list))
 
-def try_strings(a,b):
-    print("Attemting to crack")
-    print(xor_strings(a,b))
-    print(or_strings(a,b))
-    print(and_strings(a,b))
-    print(del_strings(a,b))
-    print(add_strings(a,b))
-    print(b)
-
-def dump_bytes(a):
-    image = Image.open(a)
-    red_band = image.split()[0]
-    for i in range(0,400):
-        for j in range(0,400):
-            if red_band.getpixel((i, j)) == 72:
-                if red_band.getpixel((i+1, j)) == 20:
-                    print(i,j)
-
+def try_strings(a,b,i):
+        print(print_inc(xor_strings(a,b),7))
+    # for z in range (0,127):
+    #     print(print_inc(xor_strings(a,b),z))
+    
 
 
 if __name__ == "__main__":
 # execute only if run as a script
-    dump_bytes("3decode.png")
-    a, b = extract_pixels("1.png", "1decode.png", 241, 260)
-    try_strings(a, b)
-    a, b = extract_pixels("2.png", "s1.png", 241, 264)
-    try_strings(a, b)
+    # #dump_bytes("3decode.png")
+    # a, b = extract_pixels("1.png", "1decode.png", 241, 260)
+    # try_strings(a, b, 0)
+    # a, b = extract_pixels("2.png", "s1.png", 241, 264) 
+    
+    # #Ajnap"jadidi."I'm"hipe0
+    # #Ahlan habibi. I'm fine., How are you, 
 
-    a,b = extract_pixels("3.png", "s2.png", 241, 253)
-    try_strings(a, b)
-    a,b = extract_pixels("2.png", "2decode.png", 241, 263)
-    try_strings(a, b)
-    a,b = extract_pixels("5.png", "s3.png", 295, 335)
-    try_strings(a, b)
-    a,b = extract_pixels("3.png", "3decode.png", 241, 253)
-    try_strings(a, b)
-    a,b = extract_pixels("4.png", "4decode.png", 241, 283)
-    try_strings(a, b)
-    a,b = extract_pixels("3.png", "s4.png", 241, 288)
-    try_strings(a, b)
-    a,b = extract_pixels("5.png", "5decode.png", 295, 335)
-    try_strings(a, b)
+    # try_strings(a, b, 1)
+    # print("----")
+    # a,b = extract_pixels("3.png", "s2.png", 241, 253)
+    # try_strings(a, b,2)
+    
+    # print("---")
+    # a,b = extract_pixels("2.png", "2decode.png", 241, 263)
+    # try_strings(a, b, -1)
+    # try_strings(a, b, 3)
+    # print("---")
+    #a,b = extract_pixels("5.png", "s3.png", 295, 335)
+    #try_strings(a, b, 4)
+    #try_strings(a, b, 4 - 14)
 
-15 
-   # extract_pixels("1decode.png", "2decode.png", 241, 263)
-   # print("New line")
-   # extract_pixels("3.png", "3decode.png", 241, 253)
-   # print("New line")
-   # extract_pixels("4.png", "4decode.png", 241, 283)
-   # print("New line")
-   # extract_pixels("5.png", "5decode.png", 295, 335)
-   # print("New line")
-   # extract_pixels("2.png", "s1.png", 241, 264)
-   # print("New line")
-  # print("New line")
-   # extract_pixels("3.png", "s4.png", 241, 288)
-   # print("New line")
+    # print("---")
+    #a,b = extract_pixels("3.png", "3decode.png", 241, 253)
+    #try_strings(a, b, 5)
+    # print("---")
+     #a,b = extract_pixels("4.png", "4decode.png", 241, 283)
+     #try_strings(a, b, 6)
+    # print("---")
+     a,b = extract_pixels("3.png", "s4.png", 241, 288) # Got damn
+     try_strings(a, b, 7)
+    # print("---")
+    
+    #a,b = extract_pixels("5.png", "5decode.png", 295, 335)
+    #try_strings(a, b, 8)
+    # print("---")
 
-#    find_text_in_image("3decode.png")
-#    find_text_in_image("decoded_20181210143035.png")
+   
